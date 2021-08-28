74f6f04b1f7db5104ba46f475dcb938fd79ea1c8
Francisco leon <22012000fdlq@gmail.com>
2021-08-27 22:17:24 -0400

    wip

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,66 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <!-- Panel central -->
+    <img class="img-banner" alt="Vue logo" src="./assets/logo.png" />
+    <h2 class="my-4">PokeGuía</h2>
+    <label class="mr-2">Nombre:</label>
+    <input type="text" placeholder="Ingrese un pokémon" v-model="pokemonName" />
+    <button @click="searchPokemon()">Buscar</button>
+    <!-- Imagen de Pokemon buscado -->
+    <div>
+      <img :src="pokemon.sprites.front_default" />
+    </div>
+    <div>
+      <h2>Movimientos</h2>
+      <p v-for="(movimiento, $index) in moves" :key="$index">
+        {{ movimiento.move.name }}
+      </p>
+    </div>
+    <div>
+      <h2>Habilidades</h2>
+      <p v-for="(habilidad, $index) in abilities" :key="$index">
+        {{ habilidad.ability.name }}
+      </p>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
-  name: "App",
-  components: {
-    HelloWorld,
+  name: 'App',
+  data: () => ({
+    pokemonName: 'pikachu',
+    pokemon: null
+  }),
+  methods: {
+    searchPokemon() {
+      console.log(this.pokemonName)
+      this.fetchPokemon()
+    },
+    fetchPokemon() {
+      fetch(`https://pokeapi.co/api/v2/pokemon/${this.pokemonName}`)
+        .then((response) => response.json())
+        .then((json) => (this.pokemon = json))
+    }
   },
-};
+  computed: {
+    moves() {
+      return this.pokemon.moves
+    },
+    abilities() {
+      return this.pokemon.abilities
+    }
+  },
+
+  created() {
+    this.searchPokemon()
+  }
+}
 </script>
 
-<style lang="scss">
+<style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
